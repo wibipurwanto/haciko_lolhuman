a5da9937a05dcba18b9adba978cc8b7c5467e02c
wibiby <138424987+wibipurwanto@users.noreply.github.com>
2023-09-12 13:45:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+require("./utils/cron");
+const {
+  default: WASocket,
+  fetchLatestBaileysVersion,
+  DisconnectReason,
+  useMultiFileAuthState,
+  fetchLatestWaWebVersion,
+} = require("@adiwajshing/baileys");
+const Pino = require("pino");
+const { sessionName } = require("./config.json");
+const { Boom } = require("@hapi/boom");
+const store = require("./store");
+const { existsSync, watchFile } = require("fs");
+const path = require("path");
+let messageHandler = require("./handler/message");
+
+existsSync("./store/baileys_store.json") &&
+  store.readFromFile("./store/baileys_store.json");
+setInterval(() => {
+  store.writeToFile("./store/baileys_store.json");
+}, 10000);
+
+watchFile("./handler/message.js", () => {
+  const dir = path.resolve("./handler/message.js");
+  if (dir in require.cache) {
+    delete require.cache[dir];
+    messageHandler = require("./handler/message");
+    console.log(`reloaded message.js`);
+  }
+});
+
+const connect = async () => {
+  const { state, saveCreds } = await useMultiFileAuthState(
+    path.resolve(`${sessionName}-session`),
+    Pino({ level: "silent" })
+  );
+  let { version, isLatest } = await fetchLatestWaWebVersion().catch(() =>
+    fetchLatestBaileysVersion()
+  );
+
+  console.log(`Using: ${version}, newer: ${isLatest}`);
+  const sock = WASocket({
+    printQRInTerminal: true,
+    auth: state,
+    logger: Pino({ level: "silent" }),
+    version,
+  });
+  store.bind(sock.ev);
+
+  sock.ev.on("chats.set", () => {
+    console.log("got chats", store.chats.all().length);
+  });
+
+  sock.ev.on("contacts.set", () => {
+    console.log("got contacts", Object.values(store.contacts).length);
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+  sock.ev.on("connection.update", async (up) => {
+    const { lastDisconnect, connection } = up;
+    if (connection) {
+      console.log("Connection Status: ", connection);
+    }
+
+    if (
+      connection === "close" &&
+      lastDisconnect?.error?.output?.statusCode !== 401
+    ) {
+      console.log("Reconnecting...");
+      connect();
+    } else if (
+      connection === "close" &&
+      lastDisconnect?.error?.output?.statusCode === 401
+    ) {
+      console.error(
+        "Session expired or replaced, please scan QR Code again..."
+      );
+    }
+  });
+
+  // messages.upsert
+  sock.ev.on("messages.upsert", ({ messages, type }) => {
+    if (type !== "notify") return;
+    messageHandler(sock, messages[0]);
+  });
+
+  process.on("uncaughtException", (err) => {
+    console.error(err?.message);
+  });
+};
+connect();
---
+++ utils/message.js
@@ -0,0 +1,140 @@
+/**
+ * @param {string} prefix
+ */
+exports.help = (prefix, time) => {
+  return `
+╠═══════════════════❍
+║        *「 Haciko Lolhuman」* 
+╠═════════════════════      
+║┏━━⊱  「 COMMAND 」
+║┃      
+║┣❏*${prefix}* command
+║┃      
+║┣━━⊱   「 MENU 」
+║┃
+║┣❏${prefix}daftar nama      
+║┣❏${prefix}help
+║┣❏${prefix}owner
+║┣❏${prefix}donate
+║┣❏${prefix}cast
+║┣❏${prefix}cektotaluser
+║┣❏${prefix}ceklimit
+║┣❏${prefix}setlimit
+║┣❏${prefix}caristiker
+║┃      
+║┣━━⊱   「 Islami 」
+║┃
+║┣❏${prefix}listsurah
+║┣❏${prefix}asmaulhusna
+║┣❏${prefix}alquran no_surah
+║┣❏${prefix}alquranaudio no_surah
+║┣❏${prefix}alquranaudio no_surah/no_ayat
+║┣❏${prefix}alquranaudio no_surah/no_ayat1-no_ayat2
+║┣❏${prefix}kisahnabi nama_nabi
+║┣❏${prefix}jadwalsholat daerah
+║┃
+║┣━━⊱   「 Stalking 」
+║┃
+║┣❏${prefix}stalkgithub
+║┣❏${prefix}stalkig
+║┣❏${prefix}stalktwitter
+║┃      
+║┣━━⊱   「 Downloader 」
+║┃
+║┣❏${prefix}ytsearch query
+║┣❏${prefix}ytplay query
+║┣❏${prefix}ytmp3 url_video
+║┣❏${prefix}ytmp32 url_video
+║┣❏${prefix}ytmp4 url_video
+║┣❏${prefix}ytmp42 url_video
+║┣❏${prefix}tiktoknowm url_video
+║┣❏${prefix}tiktokmusic url_video
+║┣❏${prefix}igdl url_post
+║┣❏${prefix}fbdl url_video
+║┣❏${prefix}jooxplay query
+║┣❏${prefix}spotify url_music
+║┣❏${prefix}spotifysearch query
+║┣❏${prefix}pinterest query
+║┣❏${prefix}pinterestdl url_pinterest
+║┣❏${prefix}pixiv query
+║┣❏${prefix}pixivdl url_pixiv
+║┣❏${prefix}zippyshare url_zippyshare
+║┣❏${prefix}telesticker url_pack
+║┃      
+║┣━━⊱   「 Movie & Story 」
+║┃
+║┣❏${prefix}drakorongoing
+║┣❏${prefix}lk21 query
+║┣❏${prefix}wattpad url_wattpad
+║┣❏${prefix}wattpadsearch query
+║┣❏${prefix}cerpen
+║┣❏${prefix}ceritahoror
+║┃      
+║┣━━⊱   「 Searching 」
+║┃
+║┣❏${prefix}shopee query
+║┣❏${prefix}gimage query
+║┣❏${prefix}google query
+║┣❏${prefix}gimage2 query
+║┣❏${prefix}konachan query
+║┣❏${prefix}playstore query
+║┣❏${prefix}stickerwa query
+║┣❏${prefix}wallpapersearch query
+║┣❏${prefix}wallpapersearch2 query
+║┃      
+║┣━━⊱   「 Random Text 」
+║┃
+║┣❏${prefix}quotes
+║┣❏${prefix}quotesdilan
+║┣❏${prefix}quotesanime
+║┣❏${prefix}quotesimage
+║┣❏${prefix}faktaunik
+║┣❏${prefix}katabijak
+║┣❏${prefix}pantun
+║┣❏${prefix}bucin
+║┣❏${prefix}randomnama
+║┃      
+║┣━━⊱   「 Information 」
+║┃
+║┣❏${prefix}jadwaltv channel
+║┣❏${prefix}jadwaltvnow
+║┣❏${prefix}jadwalbola
+║┣❏${prefix}heroml hero_name
+║┣❏${prefix}wikipedia query
+║┣❏${prefix}translate kode_negara text
+║┣❏${prefix}brainly query
+║┣❏${prefix}newsinfo
+║┣❏${prefix}cnnindonesia
+║┣❏${prefix}cnnnasional
+║┣❏${prefix}cnninternasional
+║┣❏${prefix}infogempa
+║┣❏${prefix}lirik query
+║┣❏${prefix}cuaca daerah
+║┣❏${prefix}kodepos query
+║┣❏${prefix}indbeasiswa
+║┣❏${prefix}hoax
+║┣❏${prefix}nsfwcheck      
+║┃      
+║┣━━⊱   「 Primbon 」
+║┃
+║┣❏${prefix}artinama name
+║┣❏${prefix}jodoh name1 & name2 
+║┣❏${prefix}weton tanggal bulan tahun
+║┣❏${prefix}jadian tanggal bulan tahun
+║┣❏${prefix}tebakumur name
+║┃      
+║┣━━⊱   「 Other 」
+║┃
+║┣❏${prefix}spamsms nomor
+║┣❏${prefix}menuimage
+║┣❏${prefix}menuanime
+║┣❏${prefix}fotofilter
+║┃      
+║┃
+║┣━━⊱*「 Thanks To 」* 
+║┃
+║┣❏ ALLAH S.W.T
+║┃
+║┗━━━⊱*《 Haciko Lolhuman 》*
+╚═════════════════❍`;
+};
